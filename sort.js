009f9ea485e90573544903f4215243569119c0fe
Frank Lam <frank.is.missing@gmail.com>
2017-11-30 20:11:53 +0000

    Completed part 2

---
+++ sort.js
@@ -0,0 +1,30 @@
+// var array = [10, 2, 5, 1, 9];
+// array.sort(function (a, b){
+//   return a - b;
+// });
+
+// console.log(array);
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function (a,b){
+  if (a.name < b.name){
+    return -1;
+  }
+
+  if (a.name > b.name){
+    return 1;
+  }
+
+  if (a.name === b.name){
+    return a.age - b.age
+  }
+
+});
+
+console.log(students);
